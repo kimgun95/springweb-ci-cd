df426a6548deded1d7fdcd668bd04b593e29b703
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2022-12-09 17:59:30 +0900

    JPA로 간단한 API 만들기 및 테스트까지

---
+++ src/test/java/com/example/springweb/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.example.springweb.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After("")
+    public void clenUp() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        // given
+        postsRepository.save(Posts.builder()
+                .title("제목")
+                .content("내용")
+                .author("작가")
+                .build());
+
+        // when
+        List<Posts> posts = postsRepository.findAll();
+
+        // then
+        Posts post = posts.get(0);
+        assertThat(post.getTitle(), is("제목"));
+        assertThat(post.getContent(), is("내용"));
+
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        // given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("제목")
+                .content("내용")
+                .author("작가")
+                .build());
+
+        // when
+        List<Posts> posts = postsRepository.findAll();
+
+        // then
+        Posts post = posts.get(0);
+        assertTrue(post.getCreateDate().isAfter(now));
+        assertTrue(post.getModifiedDate().isAfter(now));
+    }
+}
