9034008b157adf26bf94188e132d35d668259223
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2022-12-14 18:29:28 +0900

    handlebars 활용 화면 구현 및 테스트

---
+++ src/test/java/com/example/springweb/service/PostsServiceTest.java
@@ -0,0 +1,49 @@
+package com.example.springweb.service;
+
+import com.example.springweb.domain.posts.Posts;
+import com.example.springweb.domain.posts.PostsRepository;
+import com.example.springweb.domain.posts.PostsSaveRequestDto;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsServiceTest {
+
+    @Autowired
+    private PostsService postsService;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void DTO데이터를_posts테이블에_저장() {
+        //given
+        PostsSaveRequestDto dto = PostsSaveRequestDto.builder()
+                .author("gun")
+                .content("just test content")
+                .title("this is title")
+                .build();
+
+        //when
+        postsService.save(dto);
+
+        //then
+        Posts posts = postsRepository.findAll().get(0);
+        assertThat(posts.getAuthor()).isEqualTo(dto.getAuthor());
+        assertThat(posts.getContent()).isEqualTo(dto.getContent());
+        assertThat(posts.getTitle()).isEqualTo(dto.getTitle());
+
+    }
+}
