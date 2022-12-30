52a240ee320aa7c1f3b5187f4c0ff77b48f3a6e8
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2022-12-30 17:40:43 +0900

    travis test는 제대로 되는 것인가

--- src/test/java/com/example/springweb/web/WebControllerTest.java
+++ src/test/java/com/example/springweb/web/WebControllerTest.java
@@ -22,6 +22,6 @@ public class WebControllerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("스프링부트로 시작하는 웹 서비스1");
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
 }
